cb301fd3a8a6915c73d518ccf7b0e13017b7768d
Francisca <francisca@LAPTOP-K3HM83UF.localdomain>
2023-03-03 22:14:32 +0000

    Added the tangram and the cube

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -2,6 +2,8 @@ import { CGFscene, CGFcamera, CGFaxis, CGFappearance } from "../lib/CGF.js";
 import { MyPyramid } from "./MyPyramid.js";
 import { MyCone } from "./MyCone.js";
 import { MyPlane } from "./MyPlane.js";
+import { MyTangram } from "./MyTangram.js";
+import { MyUnitCube } from "./MyUnitCube.js";
 
 /**
 * MyScene
@@ -30,11 +32,14 @@ export class MyScene extends CGFscene {
         this.plane = new MyPlane(this, 5);
         this.cone = new MyCone(this, 3, 1);
         this.pyramid = new MyPyramid(this, 3, 1);
+        this.tangram = new MyTangram(this);
+        this.unitCube = new MyUnitCube(this);
+
         
-        this.objects = [this.plane, this.pyramid, this.cone];
+        this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.unitCube];
 
         // Labels and ID's for object selection on MyInterface
-        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2};
+        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram': 3, 'Unit Cube': 4};
 
         //Other variables connected to MyInterface
         this.selectedObject = 0;
@@ -43,6 +48,7 @@ export class MyScene extends CGFscene {
         this.displayNormals = false;
         this.objectComplexity = 0.5;
         this.scaleFactor = 2.0;
+        this.globalAmbientLight = 0.5;
 
     }
     initLights() {
@@ -152,6 +158,8 @@ export class MyScene extends CGFscene {
         this.loadIdentity();
         // Apply transformations corresponding to the camera position relative to the origin
         this.applyViewMatrix();
+
+        this.setGlobalAmbientLight(this.globalAmbientLight, this.globalAmbientLight, this.globalAmbientLight, 1.0);
         
         this.lights[0].update();
         this.lights[1].update();
---
+++ tp3/MyTangram.js
@@ -0,0 +1,141 @@
+import {CGFobject} from '../lib/CGF.js';
+import { MyDiamond } from "./MyDiamond.js";
+import { MyTriangle } from "./MyTriangle.js";
+import { MyParallelogram } from "./MyParallelogram.js";
+import { MyTriangleSmall } from "./MyTriangleSmall.js";
+import { MyTriangleBig } from "./MyTriangleBig.js";
+/**
+ * MyTangram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyTangram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+                
+	
+        this.diamond = new MyDiamond(this.scene);
+        this.triangle = new MyTriangle(this.scene);
+        this.parallelogram = new MyParallelogram(this.scene);
+        this.triangleSmall = new MyTriangleSmall(this.scene);
+        this.triangleBig = new MyTriangleBig(this.scene);
+        this.triangleSmallRed =new MyTriangleSmall(this.scene);
+        this.triangleOrange = new MyTriangle(this.scene);
+   
+	}
+	
+	display() {
+                // Diamond
+
+                var matrixTranslate = [1,0,0,0,
+                                   0,1,0,0,
+                                   0,0,1,0,
+                                   0,1,0,1];
+
+                this.scene.pushMatrix();                   
+                this.scene.multMatrix(matrixTranslate);
+            
+                //this.translate(0,1,0);
+                this.diamond.display();
+                this.scene.popMatrix();
+
+                // Parallelogram
+            
+                var matrixRotate = [Math.cos(Math.PI),0,-Math.sin(Math.PI),0,
+                                    0,1,0,0,
+                                    Math.sin(Math.PI),0,Math.cos(Math.PI),0,
+                                    0,0,0,1];
+            
+                this.scene.pushMatrix();
+                this.scene.multMatrix(matrixRotate);
+            
+                this.parallelogram.display();
+                this.scene.popMatrix();
+            
+                // Small Triangle purple
+
+                var matrixTranslate = [1,0,0,0,
+                                       0,1,0,0,
+                                       0,0,1,0,
+                                       1,0,0,1];
+                
+                this.scene.pushMatrix();                       
+                this.scene.multMatrix(matrixTranslate);
+                this.triangleSmall.display();
+                this.scene.popMatrix();
+           
+                // Big Triangle Blue
+
+                var matrixTranslate = [1,0,0,0,
+                                       0,1,0,0,
+                                       0,0,1,0,
+                                       0,-3,0,1];
+            
+                this.scene.pushMatrix();
+                this.scene.multMatrix(matrixTranslate);
+                this.triangleBig.display();
+                this.scene.popMatrix();
+            
+                // Triangle pink
+
+                var matrixTranslate_x = [1,0,0,0,
+                                         0,1,0,0,
+                                         0,0,1,0,
+                                         -1,0,0,1];
+            
+                var matrixTranslate_y = [1,0,0,0,
+                                         0,1,0,0,
+                                         0,0,1,0,
+                                         0,2.4,0,1];                             
+            
+                var matrixRotate = [Math.cos(Math.PI/4),Math.sin(Math.PI/4),0,0,
+                                    -Math.sin(Math.PI/4),Math.cos(Math.PI/4),0,0,
+                                    0,0,1,0,
+                                    0,0,0,1];
+                this.scene.pushMatrix();
+                this.scene.multMatrix(matrixTranslate_x);
+                this.scene.multMatrix(matrixTranslate_y);
+                this.scene.multMatrix(matrixRotate);
+                this.triangle.display();
+                this.scene.popMatrix();
+            
+                // Small triangle red
+
+                var matrixTranslateS = [1,0,0,0,
+                                        0,1,0,0,
+                                        0,0,1,0,
+                                        2,-2,0,1];
+            
+                var matrixRotateS = [Math.cos(-Math.PI/2),Math.sin(-Math.PI/2),0,0,
+                                    -Math.sin(-Math.PI/2),Math.cos(-Math.PI/2),0,0,
+                                    0,0,1,0,
+                                    0,0,0,1];
+                
+            
+                this.scene.pushMatrix();                       
+                this.scene.multMatrix(matrixRotateS);
+                this.scene.multMatrix(matrixTranslateS);
+                this.triangleSmallRed.display();
+                this.scene.popMatrix();
+            
+                // Triangle orange
+
+                var matrixRotateT = [Math.cos(Math.PI),Math.sin(Math.PI),0,0,
+                                    -Math.sin(Math.PI),Math.cos(Math.PI),0,0,
+                                    0,0,1,0,
+                                    0,0,0,1];
+            
+                var matrixTranslateT = [1,0,0,0,
+                                      0,1,0,0,
+                                      0,0,1,0,
+                                      -0.5,1.5,0,1];
+            
+                this.scene.pushMatrix();                       
+                this.scene.multMatrix(matrixRotateT);
+                this.scene.multMatrix(matrixTranslateT);
+                this.scene.scale(1.5, 1.5, 1.5);
+                this.triangleOrange.display();
+                this.scene.popMatrix();
+            
+	}
+}
