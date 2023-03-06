06f7712b315b31170758d2288480b4057b01ac9c
Francisca <francisca@LAPTOP-K3HM83UF.localdomain>
2023-03-06 15:22:39 +0000

    Added the cube normal vectors

--- tp3/MyUnitCube.js
+++ tp3/MyUnitCube.js
@@ -22,33 +22,63 @@ export class MyUnitCube extends CGFobject {
 			0.5, -0.5, 0.5    //7
 		];
 
+		this.vertices = this.vertices.concat(this.vertices);
+        this.vertices = this.vertices.concat(this.vertices);
+
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2, //tras
-			2, 1, 0,
 			1, 3, 2, //tras
-			2, 3, 1,
             4, 5, 6, //frente
-			6, 5, 4,
             5, 7, 6, //frente
-			6, 7, 5,
             0, 1, 4, //lado esq
-			4, 1, 0,
             1, 5, 4, //lado esq
-			4, 5, 1,
             0, 4, 2, //cima
-			2, 4, 0,
             4, 6, 2, //cima
-			2, 6, 4,
             5, 1, 7, //baixo
-			7, 1, 5,
             1, 3, 7, //baixo
-			7, 3, 1,
             6, 2, 7, //direito
-			7, 2, 6,
             2, 3, 7,  //direito
 			7, 3, 2,
+			7, 2, 6,
+			7, 3, 1,
+			7, 1, 5,
+			2, 6, 4,
+			2, 4, 0,
+			4, 5, 1,
+			4, 1, 0,
+			6, 7, 5,
+			6, 5, 4,
+			2, 3, 1,
+			2, 1, 0,
 		];
+
+		this.normals = [
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+		    -1, 0, 0,
+			-1, 0, 0,
+			1, 0, 0,
+			1, 0, 0,
+			0, -1, 0,
+			0, -1, 0,
+			0, 1, 0,
+			0, 1, 0,
+			0, -1, 0,
+			0, -1, 0,
+			0, 1, 0,
+			0, 1, 0,
+			1, 0, 0,
+			1, 0, 0,
+			-1, 0, 0,
+			-1, 0, 0
+		]
 
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
